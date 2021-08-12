99d66aa4379740371430530f48ef5aab85d421eb
hankh0412 <hankh0412@naver.com>
2021-08-12 21:58:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# hacking-spring-boot-reactive-test
---
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/HackingSpringBootReactiveTestApplication.java
@@ -0,0 +1,13 @@
+package com.greglturnquist.hackingspringboot.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingSpringBootReactiveTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingSpringBootReactiveTestApplication.class, args);
+	}
+
+}
